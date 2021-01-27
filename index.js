061ea3d364179d471443f3b44b508fa4e19c1595
NithinGamer <74695328+NithinGamer@users.noreply.github.com>
2021-01-27 15:32:05 +0530

    src
    src

---
+++ index.js
@@ -0,0 +1,147 @@
+import React,{useState} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+function Nithin(){
+  /*const [name,setName]=useState("Nithin M");
+  const [course,setCourse]=useState("B.Tech.IT(pursuing)");
+  const [col,setCol]=useState("SREC");
+  const [prof,setProf]=useState("Web Developer");
+  const [mail,setMail]=useState("SREC ID: nithin.1905057@srec.ac.in");*/
+  
+  /*function changeByf(e){
+    setName(e.target.value);
+    e.preventDefault();
+  }
+  function changeBys(e){
+    setCourse(e.target.value);
+  }
+  function changeByt(e){
+    setCol(e.target.value);
+  }
+  function changeByfo(e){
+    setProf(e.target.value);
+  }
+  function changeByfi(e){
+    setMail(e.target.value);
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+
+  const changeByf=(e)=>{setName(e.target.value);}
+  const changeBys=(e)=>{setCourse(e.target.value);}
+  const changeByt=(e)=>{setCol(e.target.value);}
+  const changeByfo=(e)=>{setProf(e.target.value);}
+  const changeByfi=(e)=>{setMail(e.target.value);}
+  const handleSubmit=(e)=>{ e.preventDefault();}*/
+
+  /*to be used within form
+  <input type="text" value={name} onChange={e=>setName(e.target.value)}/><hr class="guk"></hr>
+        <input type="text" value={course} onChange={e=>setCourse(e.target.value)}/><hr class="guk"></hr>
+        <input type="text" value={col} onChange={e=>setCol(e.target.value)}/><hr class="guk"></hr>
+        <input type="text" value={prof} onChange={e=>setProf(e.target.value)}/><hr class="guk"></hr>
+        <input type="text" value={mail} onChange={e=>setMail(e.target.value)}/><hr class="guk"></hr>
+        <button type="submit" class="nurture">Change</button>*/
+  return (<center>
+    <div class="box">
+     <img class="rez" src="https://d37b3blifa5mva.cloudfront.net/000_clients/527999/page/5279993mGY8fFL.jpg"></img>
+     <input type="file" name="" id="file" accept="image/*"></input>
+     <h1 class="poz">
+      <form>
+        <h1 class="poz">Nithin M<hr></hr>B.Tech.IT(pursuing)<hr></hr>SREC<hr></hr>Web Developer<hr></hr>SREC ID:
+        nithin.1905057@srec.ac.in<hr></hr>
+        </h1>
+      </form>
+      <a href="https://www.facebook.com"><img class="sizer" src="https://image.freepik.com/free-icon/facebook-logo-with-rounded-corners_318-9850.jpg"></img></a>
+      <a href="https://www.instagram.com"><img class="sizer" src="https://cdn.freebiesupply.com/images/large/2x/instagram-logo-white-on-black.png"></img></a>
+      <a href="https://www.linkedin.com"><img class="sizer" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-linkedin-3.png"></img></a>
+      <a href="https://www.twitter.com"><img class="sizer" src="https://icon-library.com/images/black-and-white-twitter-icon/black-and-white-twitter-icon-11.jpg"></img></a>
+     </h1>
+    </div>
+    </center>
+    );
+}
+
+function Nithish(){
+  return (<center>
+    <div class="box">
+     <img class="rez" src="https://bsmedia.business-standard.com/_media/bs/img/article/2019-04/01/full/1554107682-975.jpg"></img>
+     <input type="file" name="" id="file" accept="image/*"></input>
+     <h1 class="poz">
+      <form>
+        <h1 class="poz">Nithish Kannan S<hr></hr>B.Tech.IT(pursuing)<hr></hr>SREC<hr></hr>Web Developer<hr></hr>SREC ID:
+        nithishkannan.1905058@srec.ac.in<hr></hr>
+        </h1>
+      </form>
+      <a href="https://www.facebook.com"><img class="sizer" src="https://image.freepik.com/free-icon/facebook-logo-with-rounded-corners_318-9850.jpg"></img></a>
+      <a href="https://www.instagram.com"><img class="sizer" src="https://cdn.freebiesupply.com/images/large/2x/instagram-logo-white-on-black.png"></img></a>
+      <a href="https://www.linkedin.com"><img class="sizer" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-linkedin-3.png"></img></a>
+      <a href="https://www.twitter.com"><img class="sizer" src="https://icon-library.com/images/black-and-white-twitter-icon/black-and-white-twitter-icon-11.jpg"></img></a>
+     </h1>
+    </div>
+    </center>
+    );
+}
+
+function Gokul(){
+  return (<center>
+    <div class="box">
+     <img class="rez" src="https://d.newsweek.com/en/full/1542247/cristiano-ronaldo.jpg?w=1600&h=1600&q=88&f=6e67ed484a6ffa5b330c8f7e704c155c"></img>
+     <input type="file" name="" id="file" accept="image/*"></input>
+     <h1 class="poz">
+      <form>
+        <h1 class="poz">Gokul Prasanth R<hr></hr>B.Tech.IT(pursuing)<hr></hr>SREC<hr></hr>Web Developer<hr></hr>SREC ID:
+        gokulprasanth.1905028@srec.ac.in<hr></hr>
+        </h1>
+      </form>
+      <a href="https://www.facebook.com"><img class="sizer" src="https://image.freepik.com/free-icon/facebook-logo-with-rounded-corners_318-9850.jpg"></img></a>
+      <a href="https://www.instagram.com"><img class="sizer" src="https://cdn.freebiesupply.com/images/large/2x/instagram-logo-white-on-black.png"></img></a>
+      <a href="https://www.linkedin.com"><img class="sizer" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-linkedin-3.png"></img></a>
+      <a href="https://www.twitter.com"><img class="sizer" src="https://icon-library.com/images/black-and-white-twitter-icon/black-and-white-twitter-icon-11.jpg"></img></a>
+     </h1>
+    </div>
+    </center>
+    );
+}
+
+function Kissore(){
+  return (<center>
+    <div class="box">
+     <img class="rez" src="https://d37b3blifa5mva.cloudfront.net/000_clients/527999/page/5279993mGY8fFL.jpg"></img>
+     <input type="file" name="" id="file" accept="image/*"></input>
+     <h1 class="poz">
+      <form>
+        <h1 class="poz">Kissore Kumar M<hr></hr>B.Tech.IT(pursuing)<hr></hr>SREC<hr></hr>Web Developer<hr></hr>SREC ID:
+        kissorekumar.1905040@srec.ac.in<hr></hr>
+        </h1>
+      </form>
+      <a href="https://www.facebook.com"><img class="sizer" src="https://image.freepik.com/free-icon/facebook-logo-with-rounded-corners_318-9850.jpg"></img></a>
+      <a href="https://www.instagram.com"><img class="sizer" src="https://cdn.freebiesupply.com/images/large/2x/instagram-logo-white-on-black.png"></img></a>
+      <a href="https://www.linkedin.com"><img class="sizer" src="https://cdns.iconmonstr.com/wp-content/assets/preview/2012/240/iconmonstr-linkedin-3.png"></img></a>
+      <a href="https://www.twitter.com"><img class="sizer" src="https://icon-library.com/images/black-and-white-twitter-icon/black-and-white-twitter-icon-11.jpg"></img></a>
+     </h1>
+    </div>
+    </center>
+    );
+}
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Nithin />
+    <Nithish />
+    <Gokul />
+    <Kissore />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
